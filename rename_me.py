133584d0956d5e8f8cfc65ec43788390f499d7b4
leoneich <leoneich@student.21-school.ru>
2024-09-05 03:01:57 +0300

    first draft of flavel

---
+++ rename_me.py
@@ -0,0 +1,23 @@
+num = int(input())
+shift = int(input())
+facts = [True] * num
+step = 1
+index = 0
+counter = num
+print(facts[2])
+while counter - 1:
+    if facts[index % num] and step < shift:
+
+        print('number survived:', index % num + 1, f'(index: {index % num}), step: {step}')
+        step += 1
+        index += 1
+    elif not facts[index % num] and step <= shift:
+        print('dead number appeared', index % num + 1, f'step = {step}')
+        index += 1
+    else:
+        step = 1
+        facts[index % num] = False
+        print('number killed', index % num + 1)
+
+        counter -= 1
+        index += 1
