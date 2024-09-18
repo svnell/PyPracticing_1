128db5abd507d5d33eee22c827dc1ff8a023d909
Nayomy <chersvyat@yandex.ru>
2024-09-18 21:17:53 +0300

    Доработать декод

--- white_blank.py
+++ white_blank.py
@@ -1,22 +1,7 @@
-#def new_format(stg, letter_to_delete):
-#return new_format
-def deleter(stringa: str, i):
-    new_stringa = stringa
-    if new_stringa == '  ':
-        return ''
-    else:
-        for s in stringa:
-            if chr(i) in new_stringa:
-                if s == chr(i):
-                    new_stringa = new_stringa.replace(s, '')
-                    return deleter(new_stringa, i + 1)
-                print(new_stringa + f' + {chr(i-1)}')
-                return deleter(new_stringa, i + 1)
-
-
-stg = input().lower() + ' запретил букву'
-i = 1072
-deleter(stg, i)
-
-
-
+n, m = [int(num) for num in input().split()]
+row = str()
+for i in range(1,  n+1):
+    for j in range(m):
+        row += str(i + j * n)+' '
+    print(row.ljust(3))
+    row = str()
