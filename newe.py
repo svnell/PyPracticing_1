20b37ba1be3f2e7d86b59f6327e3f7b2cd860962
leoneich <leoneich@student.21-school.ru>
2024-09-18 22:55:44 +0300

    неплохое решение обозначения хода коня

--- newe.py
+++ newe.py
@@ -1,40 +1,17 @@
-def rek(matrix, n, m, shift):
-    counter = 0
-    while counter < n:
-        if counter % 2 == 0:
-            for i in range(counter, n):
-                for j in range(counter, m):
-                    if i == 0 + counter // 2 or j == m - 1 - counter // 2:
-                        matrix[i][j] += shift
-                        shift += 1
-                        #print(matrix[i][j])
-                print(matrix[i], "check right")
+n = int(input())
+matrix1 = [[int(x) for x in input().split()] for _ in range(n)]
+res = [[0 for q in range(n)] for w in range(n)]
+reslt = 0
+mirror = matrix1
 
-        else:
-            print(counter)
-            for i in range(n - 1 - counter // 2, 0, -1):
-                for j in range(m - 1 - counter % 2, -1, -1):
-                    if i == n - 1 - counter // 2 or j == 0 + counter // 2:
-                        matrix[i][j] += shift
-                        shift += 1
-                print(matrix[i], "check left")
-
-        counter += 1
-
-        #else:
-        #    for i in range(n-1, counter, -1):
-        #        for j in range(m - 1, counter, -1):
-        #            if i == n - counter or j == counter - 1:
-        #                matrix[i][j] += shift
-        #                shift += 1
+for _ in range(int(input()) - 1):
+    for i in range(n):
+        for j in range(n):
+            for k in range(n):
+                res[i][j] += mirror[i][k] * matrix1[k][j]
+    mirror = res
+    res = [[0 for q in range(n)] for w in range(n)]
 
 
-    return matrix
-
-
-
-rows, columns = [int(num) for num in input().split()]
-addition = 1
-matritsa = [[int() for _ in range(columns)] for _ in range(rows)]
-
-fin = rek(matritsa, rows, columns, addition)
+for _ in range(n):
+    print(*mirror[_])
